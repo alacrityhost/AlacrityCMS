738116d111982797824d8fc638850b7b2f277822
unknown <jfaucher@.(none)>
2012-01-17 11:24:53 -0500

    Config updated

---
+++ config/upload.php
@@ -0,0 +1,6 @@
+<?php
+$config['upload_path'] = '/public/uploads/';
+$config['allowed_types'] = 'gif|jpg|png';
+$config['max_size']	= '2048'; // 2M max size
+$config['max_width'] = '1024'; // 1024x
+$config['max_height'] = '768';
